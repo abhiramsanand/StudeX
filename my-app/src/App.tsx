ff1f93c7153c0835fb384cdf62e7ccac7f25e9f8
abhiramsanand <abhiramcr7003@gmail.com>
2024-12-17 14:28:06 +0530

    done

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentsTable from "../src/components/studentTable";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <StudentsTable />
+    </div>
+  );
+};
+
+export default App;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
-import courseRouter from './routes/courseRouter'
-import classRouter from './routes/classRouter'
-import studentRouter from './routes/studentRouter'
+import cors from "cors";
+import courseRouter from './routes/courseRouter';
+import classRouter from './routes/classRouter';
+import studentRouter from './routes/studentRouter';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 
