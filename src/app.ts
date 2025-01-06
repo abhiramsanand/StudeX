f476332b15df1bb47d7e7ba40d0ea5fdeb384184
abhiramsanand <abhiramcr7003@gmail.com>
2025-01-06 15:53:07 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,36 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import cors from "cors";
-import courseRouter from './routes/courseRouter';
-import classRouter from './routes/classRouter';
-import studentRouter from './routes/studentRouter';
-import messagesRouter from './routes/messagesRouter';
-import authRouter from './routes/authRouter';
-import { authenticateToken } from './middlewares/authMiddleware';
+import courseRouter from "./routes/courseRouter";
+import classRouter from "./routes/classRouter";
+import studentRouter from "./routes/studentRouter";
+import messagesRouter from "./routes/messagesRouter";
+import authRouter from "./routes/authRouter";
+import { authenticateToken } from "./middlewares/authMiddleware";
+import { scheduledTask } from "./config/scheduledTasks";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
-app.use('/api/courses', authenticateToken, courseRouter);
-app.use('/api/classes', authenticateToken, classRouter);
-app.use('/api/students', authenticateToken, studentRouter);
-app.use('/api/messages', authenticateToken, messagesRouter)
+app.use("/api/courses", authenticateToken, courseRouter);
+app.use("/api/classes", authenticateToken, classRouter);
+app.use("/api/students", authenticateToken, studentRouter);
+app.use("/api/messages", authenticateToken, messagesRouter);
+
+scheduledTask();
 
 export default app;
---
+++ src/config/scheduledTasks.ts
@@ -0,0 +1,27 @@
+import cron from "node-cron";
+import { Students } from "../models/students";
+import { noCourses } from "./noCourses";
+
+export const scheduledTask = () => {
+  cron.schedule("* * * * *", async () => {
+    try {
+      const studentsWithoutCourses = await Students.find(
+        { coursesselected: { $size: 0 } },
+        "email student_name -_id"
+      );
+
+      if (studentsWithoutCourses.length > 0) {
+        for (const student of studentsWithoutCourses) {
+          const studentName = student.student_name;
+          const studentEmail = student.email;
+
+          await noCourses(studentName, studentEmail);
+        }
+      } else {
+        console.log("No students found without selected courses.");
+      }
+    } catch (error) {
+      console.error("Error fetching students without courses:", error);
+    }
+  });
+};
