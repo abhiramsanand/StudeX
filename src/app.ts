7d6e48e842273185e6fc216b6b928608e0c4a905
abhiramsanand <abhiramcr7003@gmail.com>
2025-01-08 20:13:38 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://stude-x.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://stude-x.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
